364b5fb3e29d868032720ca1cfac73f7e9303761
Cherise Foster <cherise.foster@outlook.com>
2023-05-19 12:36:24 -0600

    attempted

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [sushiDisplayed, setSushiDisplay] = useState(0);
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(data => setSushis(data))
+  }, [])
+
+  function handleClick() {
+    setSushiDisplay((sushiDisplayed + 4) % sushis.length);
+  }
+
+  function handleEaten(id) {
+    console.log(id)
+    setSushis(sushis.map(sushi => sushi.id === id ? {...sushi, eaten:true} : sushi 
+      )
+    );
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis.slice(sushiDisplayed, sushiDisplayed + 4)}
+      handleClick={handleClick}
+      handleEaten={handleEaten} />
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, handleClick, handleEaten}) {
+  const renderSushis = sushis.map((sushi) => {
+    return (
+      <Sushi key={sushi.id} sushi={sushi} handleEaten={handleEaten}/>)
+  })
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushis}
+      <MoreButton handleClick={handleClick}/>
     </div>
   );
 }
